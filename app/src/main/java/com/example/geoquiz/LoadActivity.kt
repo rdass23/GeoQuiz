206c35d3c0f6041c3176de9aeed950a26a1f3ac5
DESKTOP-919GTBV\H <hs588187@dal.ca>
2022-03-16 22:16:05 +0800

    Have loading screen switch to main screen

--- app/src/main/java/com/example/geoquiz/LoadActivity.kt
+++ app/src/main/java/com/example/geoquiz/LoadActivity.kt
@@ -3,18 +3,26 @@ package com.example.geoquiz
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.widget.ProgressBar
 
 class LoadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load)
-        val progressbar = findViewById<ProgressBar>(R.id.progressBar)
-        progressbar.max = 100
-        val currentProgress = 100
-        ObjectAnimator.ofInt(progressbar, "progress", currentProgress)
-            .setDuration(2000)
-            .start()
+        val bar = findViewById<ProgressBar>(R.id.progressBar)
+        
+        bar.max = 100
+        val animation = ObjectAnimator.ofInt(bar, "progress", 100)
+        animation.duration = 2000
+        animation.start()
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            // TODO: check for username
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
